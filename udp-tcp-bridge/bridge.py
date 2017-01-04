4ce7aeac559b52171306ad4c42246be2498a98b7
Daniel Lofaro <danlofaro.com>
2017-01-04 11:46:32 -0800

    modified bridge to have two threads

--- udp-tcp-bridge/bridge.py
+++ udp-tcp-bridge/bridge.py
@@ -4,6 +4,7 @@ from thread import *
  
 HOST = ''   # Symbolic name meaning all available interfaces
 PORT = 8889 # Arbitrary non-privileged port
+PORT_HEART = 8890 # heart rate
 
 robot_message = "none"
 
@@ -40,6 +41,34 @@ print 'Socket bind complete'
 #Start listening on socket
 s.listen(10)
 print 'Socket now listening'
+
+
+
+
+
+
+
+sh = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+print 'Socket created'
+ 
+#Bind socket to local host and port
+try:
+    sh.bind((HOST, PORT_HEART))
+except socket.error as msg:
+    print 'Bind failed. Error Code : ' + str(msg[0]) + ' Message ' + msg[1]
+    sys.exit()
+     
+print 'Socket bind complete'
+ 
+#Start listening on socket
+sh.listen(10)
+print 'Socket now listening'
+
+
+
+
+
+
  
 #Function for handling connections. This will be used to create threads
 def clientthread(conn):
@@ -124,13 +153,28 @@ def udpClientThread(conn):
               joy_right_y = float(ds[3])              
       print "received message:", data
       
-
-while 1:
+def heartThread():
+  while 1:
     #wait to accept a connection - blocking call
-    conn, addr = s.accept()
+    conn, addr = sh.accept()
+    
     print 'Connected with ' + addr[0] + ':' + str(addr[1])
-     
+    
     #start new thread takes 1st argument as a function name to be run, second is the tuple of arguments to the function.
     start_new_thread(clientthread ,(conn,))
- 
-s.close()
+  sh.close()
+
+
+def joyThread():
+  while 1:
+    #wait to accept a connection - blocking call
+    conn, addr = s.accept()
+    
+    print 'Connected with ' + addr[0] + ':' + str(addr[1])
+    
+    #start new thread takes 1st argument as a function name to be run, second is the tuple of arguments to the function.
+    start_new_thread(clientthread ,(conn,))
+  s.close()
+
+start_new_thread(heartThread())
+start_new_thread(joyThread())
