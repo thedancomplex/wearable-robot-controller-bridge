039f1eec1652632f5fea7fb95f7d43f41596ddbe
Daniel Lofaro <danlofaro.com>
2017-01-04 21:47:34 -0800

    reading heart rate

---
+++ lights/demo/withHeart.py
@@ -0,0 +1,47 @@
+from phue import Bridge
+import numpy as np
+
+import socket
+import time
+b = Bridge('192.168.0.105')
+b.connect()
+b.get_api()
+
+bed_left = 3
+bed_right = 4
+table = 5
+
+the_max = 255
+the_min = 100
+T = 2.0
+
+while True:
+  host = "104.131.47.73"
+  port = 8890
+     
+  mySocket = socket.socket()
+  mySocket.connect((host,port))
+    
+  while True:
+      message = "req heart rate"
+      mySocket.send(message.encode())
+      data = mySocket.recv(1024).decode()
+      #data = mySocket.recv(1024)
+      print(data)
+      #time.sleep(0.25)
+      
+      split_data = data.split(" ")
+      
+      T = float(split_data[2]) / 60.0 * 2.0
+      print T
+      
+      b.set_light(bed_left,  'bri', the_max)
+      b.set_light(bed_right, 'bri', the_max)
+      b.set_light(table,     'bri', the_max)
+      time.sleep(T/2.0)
+  
+      b.set_light(bed_left,  'bri', the_min)
+      b.set_light(bed_right, 'bri', the_min)
+      b.set_light(table,     'bri', the_min)
+      time.sleep(T/2.0)
+  break
